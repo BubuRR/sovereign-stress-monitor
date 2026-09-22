91a6e0b59e7502f094b2a2090378457deee7e064
BubuRR <129631434+BubuRR@users.noreply.github.com>
2026-09-22 21:07:24 +0300

    Delete start-simple.bat

--- start-simple.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Running data cycle...
-python ssm_core.py
-echo.
-echo Starting dashboard at http://localhost:8501
-python -m streamlit run interface/app.py --server.port 8501
-pause
